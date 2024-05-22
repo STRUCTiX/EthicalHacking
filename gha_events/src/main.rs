dc97fb844e8ae03806251c095a5a2fdc1d3694fe
structix <janek@schoff.it>
2024-05-22 22:22:58 +0200

    Attempt to provide session cookie store

--- gha_events/src/main.rs
+++ gha_events/src/main.rs
@@ -1,7 +1,8 @@
-use std::env;
+use std::{env, io::Read, sync::Arc};
 
 use futures::future::join_all;
 use regex::Regex;
+use tokio::fs;
 
 #[tokio::main]
 async fn main() {
@@ -20,9 +21,13 @@ async fn main() {
     let job_ids = retrieve_job_commit_ids(run_sites).await.unwrap();
     println!("job_ids: {}", job_ids.len());
 
-    for (run, job, commit) in job_ids {
+    for (run, job, commit) in &job_ids {
         println!("Run: {run}, Job: {job}, Commit: {commit}");
     }
+
+    let job_ids = vec![("s".into(), "a".into(), "asdf".into())];
+
+    download_log(&project_name, job_ids).await.unwrap();
 }
 
 /// Get html from Github project. Each element in the vector is a pagination site.
@@ -74,6 +79,7 @@ async fn retrieve_run_urls(
     Ok(urls)
 }
 
+/// Get the run site for each action
 async fn get_run_sites(action_urls: Vec<(String, String)>) -> Result<Vec<String>, reqwest::Error> {
     let mut futures = Vec::with_capacity(25);
     for (url, _) in action_urls {
@@ -90,6 +96,7 @@ async fn get_run_sites(action_urls: Vec<(String, String)>) -> Result<Vec<String>
     Ok(results)
 }
 
+/// Retrieves the run-id, job-id and commit hash from the run sites
 async fn retrieve_job_commit_ids(
     run_sites: Vec<String>,
 ) -> Result<Vec<(String, String, String)>, regex::Error> {
@@ -100,25 +107,71 @@ async fn retrieve_job_commit_ids(
 
     let mut job_ids: Vec<(String, String, String)> = Vec::with_capacity(25);
     for r in run_sites {
-        match re.captures(&r) {
-            Some(capgroups) => {
-                let run_id = &capgroups["runid"];
-                let job_id = &capgroups["jobid"];
+        if let Some(capgroups) = re.captures(&r) {
+            let run_id = &capgroups["runid"];
+            let job_id = &capgroups["jobid"];
 
-                // Only check the commit capture if the first was successful
-                if let Some(capcommit) = re_commit.captures(&r) {
-                    let commit = &capcommit["commit"];
+            // Only check the commit capture if the first was successful
+            if let Some(capcommit) = re_commit.captures(&r) {
+                let commit = &capcommit["commit"];
 
-                    job_ids.push((run_id.into(), job_id.into(), commit.into()));
-                } else {
-                    // These runs were triggered by a pull request from a foreign repository.
-                    // Therefore, there is no commit id
-                    eprintln!("{run_id}, {job_id}, Commit not found.");
-                }
+                job_ids.push((run_id.into(), job_id.into(), commit.into()));
+            } else {
+                // These runs were triggered by a pull request from a foreign repository.
+                // Therefore, there is no commit id
+                eprintln!("{run_id}, {job_id}, Commit not found.");
             }
-            None => eprintln!("Run-ID and Job-ID not found"),
+        } else {
+            eprintln!("Run-ID and Job-ID not found")
         }
     }
 
     Ok(job_ids)
+}
+
+async fn download_log(
+    project_name: &str,
+    run_job_commit: Vec<(String, String, String)>,
+) -> Result<(), reqwest::Error> {
+    // Build a client with the Github session
+    let cookie_store = {
+        let file = std::fs::File::open("session.json")
+            .map(std::io::BufReader::new)
+            .unwrap();
+        // use re-exported version of `CookieStore` for crate compatibility
+        reqwest_cookie_store::CookieStore::load_json(file).unwrap()
+    };
+    let cookie_store = reqwest_cookie_store::CookieStoreMutex::new(cookie_store);
+    let cookie_store = Arc::new(cookie_store);
+
+    let client = reqwest::Client::builder()
+        .cookie_provider(Arc::clone(&cookie_store))
+        .build()
+        .unwrap();
+
+    let mut files = Vec::with_capacity(25);
+    for (run, job, commit) in run_job_commit {
+        let log_url =
+            format!("https://github.com/{project_name}/commit/{commit}/checks/{job}/logs");
+
+        let text = client.get(log_url).send().await?.text().await?;
+        files.push(fs::write(format!("./out/{run}_{job}_{commit}.txt"), text));
+    }
+
+    // Create directory
+    if let Err(e) = fs::create_dir("./out").await {
+        eprintln!("{e}");
+    }
+
+    // Write all files
+    let results = join_all(files).await;
+
+    // Print all file errors
+    results.iter().for_each(|x| {
+        if let Err(e) = x {
+            eprintln!("{e}")
+        }
+    });
+
+    Ok(())
 }

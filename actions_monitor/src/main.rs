8e191ebbced5e3ee4bee2dfc94ac120c561ca822
structix <janek@schoff.it>
2024-07-12 20:58:52 +0200

    Retrieve current log from Github

--- actions_monitor/src/main.rs
+++ actions_monitor/src/main.rs
@@ -1,10 +1,10 @@
 mod zip;
 
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, sync::Arc, time::Duration};
 
 use reqwest::{header, Client};
 use serde_json::Value;
-use tokio::fs;
+use tokio::{fs, time::sleep};
 use tracing::{info, warn};
 
 macro_rules! authclient {
@@ -36,6 +36,32 @@ macro_rules! authclient {
     }};
 }
 
+macro_rules! reqauthclient {
+    ($s:literal) => {{
+        static RE: once_cell::sync::OnceCell<reqwest::Client> = once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| {
+            // Build a client with the Github session
+            let cookie_store = {
+                let file = std::fs::File::open($s)
+                    .map(std::io::BufReader::new)
+                    .unwrap();
+
+                // use re-exported version of `CookieStore` for crate compatibility
+                reqwest_cookie_store::CookieStore::load_json(file).unwrap()
+            };
+            let cookie_store = reqwest_cookie_store::CookieStoreMutex::new(cookie_store);
+            let cookie_store = Arc::new(cookie_store);
+
+            // Create a reqwest client with the Github user session cookie
+            let client = reqwest::Client::builder()
+                .cookie_provider(Arc::clone(&cookie_store))
+                .build()
+                .unwrap();
+            return client;
+        })
+    }};
+}
+
 async fn get_repo_workflows(
     client: &Client,
     owner: &str,
@@ -44,6 +70,22 @@ async fn get_repo_workflows(
     Ok(client
         .get(format!(
             "https://api.github.com/repos/{owner}/{repo}/actions/runs"
+        ))
+        .send()
+        .await?
+        .json::<BTreeMap<String, Value>>()
+        .await?)
+}
+
+async fn get_workflow_jobs(
+    client: &Client,
+    owner: &str,
+    repo: &str,
+    run_id: i64,
+) -> anyhow::Result<BTreeMap<String, Value>> {
+    Ok(client
+        .get(format!(
+            "https://api.github.com/repos/{owner}/{repo}/actions/runs/{run_id}/jobs"
         ))
         .send()
         .await?
@@ -77,6 +119,27 @@ async fn download_workflow_logs(
     Ok(())
 }
 
+/// This function requires a valid Github session cookie
+pub async fn get_running_log(
+    owner: &str,
+    repo: &str,
+    commit_sha: &str,
+    job_id: i64,
+    log_num: u32,
+) -> anyhow::Result<String> {
+    let client = reqauthclient!("session.json");
+
+    let url = format!(
+        "https://github.com/{owner}/{repo}/commit/{commit_sha}/checks/{job_id}/logs/{log_num}"
+    );
+
+    info!(url);
+
+    let log = client.get(&url).send().await?.text().await?;
+
+    Ok(log)
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
@@ -100,34 +163,82 @@ async fn main() -> anyhow::Result<()> {
     info!("Repository: {repo}");
 
     let client = authclient!(token);
-    let repo_workflows = get_repo_workflows(client, &owner, &repo).await?;
-    let runs = repo_workflows.get("workflow_runs").unwrap();
+    loop {
+        let repo_workflows = get_repo_workflows(client, &owner, &repo).await?;
+        let runs = repo_workflows.get("workflow_runs").unwrap();
 
-    if let Some(wfruns) = runs.as_array() {
-        // Retrieve the run IDs
-        let mut ids = wfruns
-            .iter()
-            .filter(|x| {
-                if let Some(name) = x.get("name") {
-                    name == "AppVeyor Docker Bridge"
-                } else {
-                    false
+        if let Some(wfruns) = runs.as_array() {
+            // Retrieve the run IDs
+            let mut ids = wfruns
+                .iter()
+                .filter(|x| {
+                    if let Some(status) = x.get("status") {
+                        status != "completed"
+                    } else {
+                        false
+                    }
+                })
+                .filter(|x| {
+                    if let Some(name) = x.get("name") {
+                        name == "AppVeyor Docker Bridge"
+                    } else {
+                        false
+                    }
+                })
+                .filter_map(|x| {
+                    if let Some(head_sha) = x.get("head_sha") {
+                        if let Some(id) = x.get("id") {
+                            if let Some(head) = head_sha.as_str() {
+                                if let Some(id) = id.as_i64() {
+                                    return Some((id, head.to_string()));
+                                }
+                            }
+                        }
+                    }
+                    None
+                })
+                .collect::<Vec<(i64, String)>>();
+
+            //if !ids.is_empty() {
+            //    // Sort descending so the most recent run will be first
+            //    ids.sort();
+            //    ids.reverse();
+            //    info!("Download workflow {}", &ids[0].to_string());
+
+            //    download_workflow_logs(client, &owner, &repo, &ids[0].to_string())
+            //        .await
+            //        .unwrap();
+            //    let filename = format!("./out/{owner}_{repo}_{}.zip", &ids[0]);
+            //    zip::extract_zip(&filename, &format!("./out/{owner}_{repo}_{}", &ids[0]));
+            //} else {
+            //    info!("All workflows are completed");
+            //}
+
+            if !ids.is_empty() {
+                ids.sort_by_key(|x| x.0);
+                ids.reverse();
+                let id = ids[0].0;
+                let commit_sha = &ids[0].1;
+                info!("Download log id: {id}, commit: {commit_sha}");
+
+                // Retrieve job id from jobs endpoint
+                let job_ids = get_workflow_jobs(client, &owner, &repo, id).await?;
+                let job_ids = job_ids.get("jobs").unwrap();
+                if let Some(job_ids) = job_ids.as_array() {
+                    let job_ids = job_ids
+                        .iter()
+                        .filter_map(|x| x.get("id"))
+                        .filter_map(|x| x.as_i64())
+                        .collect::<Vec<i64>>();
+
+                    // Download the current log with the cookie session
+                    let log = get_running_log(&owner, &repo, commit_sha, job_ids[0], 2).await?;
+                    info!("{log}");
                 }
-            })
-            .filter_map(|x| x.get("id"))
-            .filter_map(|x| x.as_i64())
-            .collect::<Vec<i64>>();
-
-        // Sort descending so the most recent run will be first
-        ids.sort();
-        ids.reverse();
-
-        download_workflow_logs(client, &owner, &repo, &ids[0].to_string())
-            .await
-            .unwrap();
-        let filename = format!("./out/{owner}_{repo}_{}.zip", &ids[0]);
-        zip::extract_zip(&filename, &format!("./out/{owner}_{repo}_{}", &ids[0]));
+            } else {
+                info!("No running job");
+            }
+        }
+        sleep(Duration::from_secs(10)).await;
     }
-
-    Ok(())
 }

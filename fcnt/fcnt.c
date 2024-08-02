c65dd2d69e7e8caee41653e6507492ae24f99d95
Daniel Eisele <daniel_eisele@gmx.de>
2024-08-02 19:49:40 +0200

    Use fewer syscalls to query directory entries

--- fcnt/fcnt.c
+++ fcnt/fcnt.c
@@ -1,20 +1,63 @@
+#ifdef __linux__
+#define _GNU_SOURCE
+
+#include <stdint.h>
+#endif
+
+#include <fcntl.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <dirent.h>
 #include <errno.h>
 
+#define BUFSIZE 10 * 1024 * 1024
+
+#ifdef __linux__
+#define getdents getdents64
+#define dirent linux_dirent64
+
+struct linux_dirent64 {
+    uint64_t d_ino;
+    int64_t d_off;
+    unsigned short d_reclen;
+    unsigned char d_type;
+    char d_name[0];
+};
+#endif
+
 int main(int argc, char *argv[]) {
     const char *path = (argc > 1? argv[1] : ".");
-    DIR *dir = opendir(path);
-    if (dir == NULL) {
+    int dir = open(path, O_RDONLY | O_DIRECTORY);
+    if (dir == -1) {
         perror(path);
         return 1;
     }
 
+    char *buf = malloc(BUFSIZE);
+    if (buf == NULL) {
+        perror("malloc()");
+        return 1;
+    }
+
     int count = 0;
-    struct dirent *ent;
-    while ((ent = readdir(dir))) {
-        if (ent->d_type == DT_REG) {
-            count++;
+    while (1) {
+        ssize_t bytes_read = getdents(dir, buf, BUFSIZE);
+        if (bytes_read == -1) {
+            perror("getdents()");
+            return 1;
+        }
+
+        if (bytes_read == 0) {
+            break;
+        }
+
+        size_t pos = 0;
+        while (pos < bytes_read) {
+            struct dirent *ent = (struct dirent *)(buf + pos);
+            if (ent->d_type == DT_REG) {
+                count++;
+            }
+            pos += ent->d_reclen;
         }
     }
 
@@ -22,8 +65,6 @@ int main(int argc, char *argv[]) {
         perror("readdir()");
         return 1;
     }
-
-    closedir(dir);
 
     printf("%d\n", count);
 

